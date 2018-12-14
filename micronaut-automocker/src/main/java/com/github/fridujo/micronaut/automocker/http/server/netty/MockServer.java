385ed03fc3c37bb2c61bba20761445f1e6de4cb4
ledoyen <ledoyen.loic@gmail.com>
2018-12-14 11:11:52 +0100

    Supply automatic mocking of Micronaut HTTP server

---
+++ micronaut-automocker/src/main/java/com/github/fridujo/micronaut/automocker/http/server/netty/MockServer.java
@@ -0,0 +1,85 @@
+package com.github.fridujo.micronaut.automocker.http.server.netty;
+
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URL;
+import javax.inject.Inject;
+import javax.inject.Singleton;
+
+import io.micronaut.context.ApplicationContext;
+import io.micronaut.context.annotation.Replaces;
+import io.micronaut.context.exceptions.ConfigurationException;
+import io.micronaut.http.server.netty.NettyHttpServer;
+import io.micronaut.runtime.ApplicationConfiguration;
+import io.micronaut.runtime.server.EmbeddedServer;
+
+/**
+ * Stub for {@link NettyHttpServer} to avoid listening on a port and exposing HTTP services.
+ *
+ * @see MockMvc
+ * @see com.github.fridujo.micronaut.automocker.Automocker
+ */
+@Singleton
+@Replaces(NettyHttpServer.class)
+public class MockServer implements EmbeddedServer {
+
+    private final String scheme = "http";
+    private final String host = "test";
+    private final int port = -1;
+    private final ApplicationContext applicationContext;
+
+    @Inject
+    public MockServer(ApplicationContext applicationContext) {
+        this.applicationContext = applicationContext;
+        System.out.println("Mocking NettyHttpServer");
+    }
+
+    @Override
+    public int getPort() {
+        return port;
+    }
+
+    @Override
+    public String getHost() {
+        return host;
+    }
+
+    @Override
+    public String getScheme() {
+        return scheme;
+    }
+
+    @Override
+    public URL getURL() {
+        try {
+            return new URL(getScheme() + "://" + getHost() + ':' + getPort());
+        } catch (MalformedURLException e) {
+            throw new ConfigurationException("Invalid server URL: " + e.getMessage(), e);
+        }
+    }
+
+    @Override
+    public URI getURI() {
+        try {
+            return new URI(getScheme() + "://" + getHost() + ':' + getPort());
+        } catch (URISyntaxException e) {
+            throw new ConfigurationException("Invalid server URL: " + e.getMessage(), e);
+        }
+    }
+
+    @Override
+    public ApplicationContext getApplicationContext() {
+        return applicationContext;
+    }
+
+    @Override
+    public ApplicationConfiguration getApplicationConfiguration() {
+        return null;
+    }
+
+    @Override
+    public boolean isRunning() {
+        return true;
+    }
+}
---
+++ micronaut-automocker/src/test/java/com/github/fridujo/micronaut/automocker/http/server/netty/MockMvcTest.java
@@ -0,0 +1,28 @@
+package com.github.fridujo.micronaut.automocker.http.server.netty;
+
+import static java.util.Optional.empty;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import io.micronaut.http.HttpMethod;
+import io.micronaut.http.HttpStatus;
+import io.micronaut.web.router.Router;
+import org.junit.jupiter.api.Test;
+
+class MockMvcTest {
+
+    private final Router router = mock(Router.class);
+    private final MockMvc mockMvc = new MockMvc(router);
+
+    @Test
+    void no_route_returns_404() {
+        when(router.route(any(), anyString())).thenReturn(empty());
+
+        ReadableHttpResponse<?> response = mockMvc.exchange(HttpMethod.GET, "/");
+
+        assertThat((Object) response.getStatus()).isEqualTo(HttpStatus.NOT_FOUND);
+    }
+}
